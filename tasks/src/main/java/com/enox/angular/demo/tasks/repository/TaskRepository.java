6211fdd96b233a3f878d64a309cd13bc1a8300d2
ALLYOUNEED\eno.ahmedspahic <eno.ahmedspahic@allyouneedfresh.de>
2018-09-04 18:00:43 +0200

    checkpoint

--- tasks/src/main/java/com/enox/angular/demo/tasks/repository/TaskRepository.java
+++ tasks/src/main/java/com/enox/angular/demo/tasks/repository/TaskRepository.java
@@ -1,5 +1,9 @@
 package com.enox.angular.demo.tasks.repository;
 
-public interface TaskRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.enox.angular.demo.tasks.domain.Task;
+
+public interface TaskRepository extends CrudRepository<Task, Long>{
 
 }
